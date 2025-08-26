80a91593172cc0fe419f9f34ec698e4412addb72
gonzalo <gomezmarinero@gmail.com>
2025-08-26 17:26:09 -0300

    Limpiando comentarios

---
+++ app/src/main/java/com/example/miiweb/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.miiweb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.example.miiweb.data.AppDatabase
+import com.example.miiweb.ui.theme.MiiwebTheme
+import androidx.compose.ui.platform.LocalContext
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val database by lazy { Room.databaseBuilder(applicationContext, AppDatabase::class.java, "libro-db").build() }
+        val libroViewModel by viewModels<LibroViewModel> { LibroViewModelFactory(database.libroDao()) }
+
+        setContent {
+
+            val navController = rememberNavController()
+
+
+            MiiwebTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold { innerPadding ->
+                        Navigation(innerPadding,navController, libroViewModel) // Pasa el viewModel a Navigation
+                    }
+                    Scaffold(
+                       topBar = { MiBarraDeAplicacion(navController,libroViewModel, LocalContext.current) }
+
+                    ) { innerPadding ->
+                        Box(modifier = Modifier
+                            .padding(innerPadding).fillMaxSize()
+                        ) {
+                            // Contenido
+                           Navigation(innerPadding,navController,libroViewModel)
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
